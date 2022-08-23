985c22066092e15f00559d3d51efd0cb3ce03e5d
pangeran-bottor <pangeranbhp@gmail.com>
2022-08-23 21:30:04 +0700

    Improve LeetCode problem 41 to use constant space

--- leetcode/problems/41_first_missing_positive.py
+++ leetcode/problems/41_first_missing_positive.py
@@ -1,11 +1,11 @@
 from typing import List
 
 
+"""
+This is O(N) time and O(N) space
+
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-
-        # O(N) time, O(N) space
-        # TODO: O(1) space
         N = len(nums)
         tmp = [0 for i in range(N)]
         for num in nums:
@@ -15,4 +15,30 @@ class Solution:
         for idx, val in enumerate(tmp):
             if val == 0:
                 return idx + 1
+        return N + 1
+"""
+
+# This is O(N) time and O(1) space
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        N = len(nums)
+        
+        # move zeros and negatives to a number larger than N
+        for idx, num in enumerate(nums):
+            if num <= 0:
+                nums[idx] = N + 1
+        
+        # mark an index as negative if it's positive
+        for idx, num in enumerate(nums):
+            abs_num = abs(num)
+            if 0 < abs_num <= N:
+                if nums[abs_num-1] > 0:
+                    nums[abs_num-1] *= -1
+        
+        # result is the first index with positive value
+        for idx, num in enumerate(nums):
+            if num > 0:
+                return idx + 1
+        
+        # else, it's N+1
         return N + 1
