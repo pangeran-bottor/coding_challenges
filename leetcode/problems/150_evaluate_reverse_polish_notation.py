d852a5d80aa851b24599ee08c5e87556076be0e0
Pangeran Bottor <pangeranbhp@gmail.com>
2022-12-17 03:03:07 +0000

    Update LeetCode problem 150

--- leetcode/problems/150_evaluate_reverse_polish_notation.py
+++ leetcode/problems/150_evaluate_reverse_polish_notation.py
@@ -3,25 +3,20 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        ans = 1
-        for token in tokens:
-            if token in "+-*/":
-                b = stack.pop()
-                a = stack.pop()
+        op_map = {
+            "+": lambda x, y: x+y,
+            "-": lambda x, y: x-y,
+            "/": lambda x, y: int(x/y),
+            "*": lambda x, y: x*y
+        }
 
-                # NOTE: using eval() would be significantly slower.
-                if token == "+":
-                    ans = (a + b)
-                if token == "-":
-                    ans = (a - b)
-                if token == "*":
-                    ans = (a * b)
-                if token == "/":
-                    ans = int(a / b)
-                stack.append(ans)
-            else:
+        stack = []
+        for token in tokens:
+            if token not in op_map:
                 stack.append(int(token))
-        if stack:
-            return stack[0]
-        return ans
+            else:
+                y = stack.pop()
+                x = stack.pop()
+                stack.append(op_map[token](x, y))
+        
+        return stack.pop()
