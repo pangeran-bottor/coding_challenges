952dd1c0897e451843a805147ece72f8787bfc19
pangeran-bottor <pangeranbhp@gmail.com>
2023-01-05 19:30:29 +0700

    Add LeetCode problem 779

---
+++ leetcode/problems/779_k_th_symbol_in_grammar.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        if k % 2 == 1:
+            return self.kthGrammar(n - 1, (k + 1) // 2)
+        return (self.kthGrammar(n - 1, (k + 1) // 2) + 1) % 2
