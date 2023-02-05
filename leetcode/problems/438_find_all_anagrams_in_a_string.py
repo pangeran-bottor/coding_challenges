eb4ead1ff56ff95de1a4fb71d7c738ebf1cf303c
pangeran-bottor <pangeranbhp@gmail.com>
2023-02-05 13:48:52 +0700

    Update LeetCode problem 438

--- leetcode/problems/438_find_all_anagrams_in_a_string.py
+++ leetcode/problems/438_find_all_anagrams_in_a_string.py
@@ -1,34 +1,31 @@
+import collections
 from typing import List
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        def substract_vecs(vec1, vec2):
-            new_vec = list(vec1)
-            for i in range(26):
-                new_vec[i] -= vec2[i]
-            return new_vec
-        
-        def is_equal_vec(vec1, vec2):
-            for i in range(26):
-                if vec1[i] != vec2[i]:
-                    return False
-            return True
-        
-        p_vec = [0] * 26
+        ans = []
+        target = collections.Counter()
         for ch in p:
-            p_vec[ord(ch)-97] += 1
-        result = []
-        maps = [[0]*26]
-        for ch in s:
-            new_vec = list(maps[-1])
-            new_vec[ord(ch)-97] += 1
-            maps.append(new_vec)
+            target[ch] += 1
 
-        for i in range(len(p), len(maps)):
-            diff_vec = substract_vecs(maps[i], maps[i-len(p)])
+        curr = collections.Counter()
+        len_p = len(p)
+        len_s = len(s)
+        if len_s < len_p:
+            return ans
+        for i in range(len_p):
+            curr[s[i]] += 1
 
-            if is_equal_vec(diff_vec, p_vec):
-                result.append(i-len(p))
-            
-        return result
+        start_idx = 0
+        while start_idx <= len_s - len_p:
+            if curr == target:
+                ans.append(start_idx)
+
+            start_idx += 1
+
+            if start_idx <= len_s - len_p:
+                curr[s[start_idx - 1]] -= 1
+                curr[s[start_idx + len_p - 1]] += 1
+
+        return ans
