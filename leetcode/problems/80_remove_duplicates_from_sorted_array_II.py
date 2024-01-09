ae762bf5d4fb5335f671077b523905745b17766b
pangeran-bottor <pangeranbhp@gmail.com>
2024-01-09 23:04:46 +0700

    Update LeetCode problem 80

--- leetcode/problems/80_remove_duplicates_from_sorted_array_II.py
+++ leetcode/problems/80_remove_duplicates_from_sorted_array_II.py
@@ -3,20 +3,9 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        result = 0
-        tmp = float("-inf")
-        limit = 0
-        
-        for idx, num in enumerate(nums):
-            if num != tmp:
-                nums[result] = num
-                result += 1
-                limit = 1 
-            else:
-                if limit < 2:
-                    nums[result] = num
-                    result += 1
-                limit += 1
-            tmp = num
-                
-        return result
+        k = 0
+        for num in nums:
+            if k < 2 or nums[k - 2] != num:
+                nums[k] = num
+                k += 1
+        return k
