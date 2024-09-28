7965daa85418f94086eb592a92df5a3f85c45561
pangeran-bottor <pangeranbhp@gmail.com>
2024-09-28 14:50:43 +0700

    Update LeetCode problem 27

--- leetcode/problems/27_remove_element.py
+++ leetcode/problems/27_remove_element.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        n = len(nums)
-        for i in range(n):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+        current = 0
+        for num in nums:
+            if num == val:
+                continue
+
+            nums[current] = num
+            current += 1
+        return current
