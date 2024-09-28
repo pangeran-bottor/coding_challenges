210bc4d8131f4315fad3c0c64c5d4cda52c1a009
pangeran-bottor <pangeranbhp@gmail.com>
2024-09-28 14:31:49 +0700

    Update LeetCode problem 228

--- leetcode/problems/228_summary_ranges.py
+++ leetcode/problems/228_summary_ranges.py
@@ -3,26 +3,15 @@ from typing import List
 
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if len(nums) == 0:
-            return []
-        
-        result = [[nums[0], None]]
-        
         N = len(nums)
-        for i in range(1, N):
-            target_idx = 0
-            if result[-1][1]:
-                target_idx += 1
-            
-            if nums[i]-result[-1][target_idx] == 1:
-                result[-1][1] = nums[i]
+        ans = []
+
+        for num in nums:
+            if not ans or num > ans[-1][-1] + 1:
+                ans.append([num])
+            elif len(ans[-1]) == 1:
+                ans[-1].append(num)
             else:
-                result.append([nums[i], None])
-        
-        M = len(result)
-        for i in range(M):
-            if result[i][1]:
-                result[i] = "->".join(map(str, result[i]))
-            else:
-                result[i] = str(result[i][0])
-        return result
+                ans[-1][-1] = num
+
+        return ["->".join(map(str, el)) for el in ans]
