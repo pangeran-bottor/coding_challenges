797f1db39031b7315ef47813846d7011a5cb2f7a
pangeran-bottor <pangeranbhp@gmail.com>
2024-10-04 14:26:41 +0700

    Update LeetCode problem 78

--- leetcode/problems/78_subsets.py
+++ leetcode/problems/78_subsets.py
@@ -3,7 +3,16 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
-        for num in nums:
-            result += [[num] + cur for cur in result]
-        return result
+        def _backtrack(idx: int, curr: list):
+            ans.append(list(curr))
+
+            for i in range(idx, n):
+                curr.append(nums[i])
+                _backtrack(i + 1, curr)
+                curr.pop()
+
+        n = len(nums)
+        ans: List[List[int]] = []
+        _backtrack(0, [])
+
+        return ans
