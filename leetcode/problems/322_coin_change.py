f132c66c71076a3228672b08633702e61f3ca688
pangeran-bottor <pangeranbhp@gmail.com>
2024-10-25 22:21:07 +0700

    Update LeetCode problem 322

--- leetcode/problems/322_coin_change.py
+++ leetcode/problems/322_coin_change.py
@@ -3,25 +3,11 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        dp = [-1] * (amount + 1)
+        dp = [float("inf") for _ in range(amount + 1)]
+        dp[0] = 0
+
         for coin in coins:
-            if coin <= amount:
-                dp[coin] = 1
+            for curr in range(coin, amount + 1):
+                dp[curr] = min(dp[curr], dp[curr - coin] + 1)
 
-        for i in range(1, amount+1):
-            if dp[i] >= 0:
-                continue
-            min_changes = float("inf")
-            min_c = -1
-            for coin in coins:
-                if i - coin >= 0:
-                    if 0 <= dp[i-coin] < min_changes:
-                        min_changes = dp[i-coin]
-                        min_c = coin
-
-            if min_c != -1:
-                dp[i] = min_changes + 1
-
-        return dp[amount]
+        return dp[amount] if dp[amount] != float("inf") else -1
