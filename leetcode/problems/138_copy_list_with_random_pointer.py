e97cc3dce1be078b6c0339d63833f293a3d33693
pangeran-bottor <pangeranbhp@gmail.com>
2024-12-08 16:32:52 +0700

    Add LeetCode problem 138

---
+++ leetcode/problems/138_copy_list_with_random_pointer.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        if not head:
+            return head
+
+        mapping = {}
+
+        cur = head
+        while cur:
+            mapping[cur] = Node(cur.val, None, None)
+            cur = cur.next
+
+        cur = head
+        while cur:
+            if cur.next:
+                mapping[cur].next = mapping[cur.next]
+            if cur.random:
+                mapping[cur].random = mapping[cur.random]
+            cur = cur.next
+
+        return mapping[head]
