bb876113754b0872e4f2348650c5081a3075d508
pangeran-bottor <pangeranbhp@gmail.com>
2024-12-16 22:40:35 +0700

    Update LeetCode problem 23

--- leetcode/problems/23_merge_k_sorted_lists.py
+++ leetcode/problems/23_merge_k_sorted_lists.py
@@ -1,28 +1,28 @@
+import heapq
 from typing import List, Optional
 
 
-# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-        
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        lists = [l for l in lists if l is not None]
-        if len(lists) == 0:
-            return None
-        
-        arr = []
-        for l in lists:
-            while l:
-                arr.append(l.val)
-                l = l.next
-        arr.sort()
-        
-        result = pointer = ListNode(0)
-        for a in arr:
-            pointer.next = ListNode(a)
-            pointer = pointer.next
-        return result.next
+        heap = []
+        for idx, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, idx, node))
+
+        new_node = ListNode()
+        curr = new_node
+
+        while heap:
+            _, idx, next_node = heapq.heappop(heap)
+            curr.next = next_node
+            curr = curr.next
+            if curr.next:
+                heapq.heappush(heap, (curr.next.val, idx, curr.next))
+
+        return new_node.next
