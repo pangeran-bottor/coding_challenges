6ff222e3d3d049565639f7893826d409a6d2d03a
pangeran-bottor <pangeranbhp@gmail.com>
2024-12-28 13:14:31 +0700

    Add LeetCode problem 110

---
+++ leetcode/problems/110_balanced_binary_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def _check(node):
+            if not node:
+                return 0
+
+            left = _check(node.left)
+            right = _check(node.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+
+        return _check(root) != -1
