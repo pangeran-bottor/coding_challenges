99757e771196a053e85de2af258907a0cde55271
pangeran-bottor <pangeranbhp@gmail.com>
2025-01-06 20:43:35 +0700

    Update LeetCode problem 1769

--- leetcode/problems/1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
+++ leetcode/problems/1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -3,22 +3,21 @@ from typing import List
 
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        N = len(boxes)
-        prefix_list = [0] * N
-        suffix_list = [0] * N
+        n = len(boxes)
+        cur_count, cur_sum = 0, 0
+        ans = [0 for _ in range(n)]
 
-        count = 0
-        for i in range(N):
-            if i != 0:
-                prefix_list[i] += (prefix_list[i-1] + count)
-            if int(boxes[i]) == 1:
-                count += 1
+        for i in range(n):
+            ans[i] += cur_count + cur_sum
+            cur_sum += cur_count
+            if boxes[i] == "1":
+                cur_count += 1
 
-        count = 0
-        for i in range(N-1, -1, -1):
-            if i != N-1:
-                suffix_list[i] += (suffix_list[i+1] + count)
-            if int(boxes[i]) == 1:
-                count += 1
-        ans = [el[0] + el[1] for el in zip(prefix_list, suffix_list)]
+        cur_count, cur_sum = 0, 0
+        for i in range(n):
+            ans[n - i - 1] += cur_count + cur_sum
+            cur_sum += cur_count
+            if boxes[n - i - 1] == "1":
+                cur_count += 1
+
         return ans
