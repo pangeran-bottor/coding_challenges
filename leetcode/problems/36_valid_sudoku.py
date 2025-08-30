dcb433e9fc4da424b8c4652d169c83e83d137bb5
pangeran-bottor <pangeranbhp@gmail.com>
2025-08-30 20:39:02 +0700

    Update LeetCode problem 36

--- leetcode/problems/36_valid_sudoku.py
+++ leetcode/problems/36_valid_sudoku.py
@@ -3,24 +3,14 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in board:
-            filled = [el for el in row if el != "."]
-            if len(filled) != len(set(filled)):
-                return False
+        values = []
+        for row in range(9):
+            for col in range(9):
+                ch = board[row][col]
+                if ch == ".":
+                    continue
 
-        for i in range(9):
-            filled = [row[i] for row in board if row[i] != "."]
-            if  len(filled) != len(set(filled)):
-                return False
-
-        for i in range(3):
-            for j in range(3):
-                filled = []
-                for row in range(3*i, 3*(i+1)):
-                    for col in range(3*j, 3*(j+1)):
-                        val = board[row][col]
-                        if val != ".":
-                            filled.append(val)
-                if len(filled) != len(set(filled)):
-                    return False
-        return True
+                values.append((row, ch))
+                values.append((ch, col))
+                values.append((row // 3, col // 3, ch))
+        return len(values) == len(set(values))
