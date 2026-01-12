50f4468b97a4daddfa43eb96491980479cf96c49
pangeran-bottor <pangeranbhp@gmail.com>
2026-01-12 15:16:10 +0700

    Update LeetCode problem 79

--- leetcode/problems/79_word_search.py
+++ leetcode/problems/79_word_search.py
@@ -1,43 +1,37 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(curr_word: str, row: int, col: int):
-            if len(curr_word) == 0:
-                return True
-
-            if row < 0 or row >= R or col < 0 or col >= C:
-                return False
-
-            if curr_word[0] != board[row][col]:
-                return False
-
-            temp = board[row][col]
-            board[row][col] = "#"
-
-            for dr, dc in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
-                nrow, ncol = row + dr, col + dc
-                if backtrack(curr_word=curr_word[1:], row=nrow, col=ncol):
-                    return True
-
-            board[row][col] = temp
+        rows, cols = len(board), len(board[0])
+        board_count = Counter(c for row in board for c in row)
+        word_count = Counter(word)
+        if any(word_count[c] > board_count.get(c, 0) for c in word_count):
             return False
 
-        R, C = len(board), len(board[0])
-        for i in range(R):
-            for j in range(C):
-                ret = backtrack(word, i, j)
-                if ret:
+        def dfs(k, r, c):
+            if k == len(word):
+                return True
+
+            if not (0 <= r < rows and 0 <= c < cols):
+                return False
+            if board[r][c] != word[k]:
+                return False
+
+            tmp = board[r][c]
+            board[r][c] = "#"
+
+            for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                if dfs(k + 1, r + dr, c + dc):
                     return True
+
+            board[r][c] = tmp
+            return False
+
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(0, i, j):
+                    return True
+
         return False
-
-
-if __name__ == "__main__":
-    board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-    word = "ABCCED"
-    # board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-    # word = "SEE"
-    # board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-    # word = "ABCB"
-    print(Solution().exist(board=board, word=word))
